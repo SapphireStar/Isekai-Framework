14287c4c173c47a941deed8b3648eccf6ba57651
Tianyi Jiang <sapphirestar_@outlook.com>
2026-01-25 22:12:11 -0700

    add binding to mainmenu

--- Assets/Scripts/UISystem/Views/Screen/MainMenuScreen.cs
+++ Assets/Scripts/UISystem/Views/Screen/MainMenuScreen.cs
@@ -1,17 +1,29 @@
 using Cysharp.Threading.Tasks;
 using Isekai.Managers;
 using Isekai.UI.ViewModels.Screens;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 namespace Isekai.UI.Views.Screens
 {
     public class MainMenuScreen : Screen<MainMenuViewModel>
     {
+        public TextMeshProUGUI Title;
+        public TextMeshProUGUI Version;
+        
         public override void OnEnterScreen()
         {
-            
+            BindingDescription bindingDescription = new BindingDescription("text", "ViewModel.Title", BindingMode.OneWay);
+            bindingDescription.TargetType = typeof(TextMeshProUGUI);
+
+            Binding binding = new Binding(ViewModel, Title, bindingDescription);
+            ViewModel.Title = "Backpack Battle";
         }
 
         public override void OnExitScreen()
@@ -20,7 +32,7 @@ namespace Isekai.UI.Views.Screens
         }
         public void OnNewGameClicked()
         {
-            LevelManager.Instance.TransitionToScene("TestBattleScene").Forget();
+            LevelManager.Instance.TransitionToScene("GameScene").Forget();
         }
         public void OnSettingsClicked()
         {
